fedf839402357d1d48583ade20261f2d47d105b7
Zeina Wady <126752492+zeinawady@users.noreply.github.com>
2024-04-16 01:31:03 +0200

    First Update Transaction.cpp

--- Transaction.cpp
+++ Transaction.cpp
@@ -1,1 +1,21 @@
 #include "Transaction.h"
+#include <iostream>
+
+Transaction::Transaction(string sender, string recipient, double amount) : sender(sender), recipient(recipient), amount(amount)
+{
+    struct tm timeinfo;
+    time(&timestamp); 
+    localtime_s(&timeinfo, &timestamp);
+    timestamp = mktime(&timeinfo);
+
+}
+
+
+//returns a formatted string representation of the transaction date
+string Transaction::getFormattedDate() const {
+    char dateString[80];
+    struct tm timeinfo;
+    localtime_s(&timeinfo, &timestamp); // Convert the time_t timestamp to a local time representation
+    strftime(dateString, 80, "%d-%m-%Y %H:%M:%S", &timeinfo); // Format the date using strftime function
+    return string(dateString);
+}
